e3eff63d621b5abd282f848a3ed37ee32a01c343
Jose Luis Allaucca Tarazona <jose_luis_07129@hotmail.com>
2022-06-23 18:07:26 -0500

    Add pokemonController and listPokemon in '/' path

---
+++ src/main/java/com/test/pokeapi/controller/PokemonController.java
@@ -0,0 +1,26 @@
+package com.test.pokeapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.test.pokeapi.dto.PageSpecieDto;
+import com.test.pokeapi.services.PokemonService;
+
+@Controller()
+@RequestMapping("/")
+public class PokemonController {
+  @Autowired
+  PokemonService pokemonService;
+
+  @GetMapping
+  public String listPokemons(Model model, @RequestParam(name="offset") Integer offset, @RequestParam(name = "limit") Integer limit){
+    PageSpecieDto pageSpecieDto = pokemonService.listPokemons(offset, limit);
+
+    model.addAttribute("response", pageSpecieDto);
+    return "index";
+  }
+}
